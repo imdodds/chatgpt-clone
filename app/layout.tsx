458b9c7b843c9b3a8d664e0a4996729d72ab4b03
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-13 14:56:37 -0400

    Add homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,15 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <div className='flex'>
+          {/* Sidebar */}
+
+          {/* Client Provider - Notification */}
+
+          <div className='bg-[#343541] flex-1'>{children}</div>
+        </div>        
+      </body>
     </html>
   )
 }

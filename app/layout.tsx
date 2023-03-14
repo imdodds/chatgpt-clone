1783efb5cf3e68b8a60befacc584e558b6180f84
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-14 14:36:59 -0400

    Add login/logout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import SideBar from '../components/SideBar';
 import { getServerSession } from 'next-auth';
 import '../styles/globals.css';
 import { authOptions } from '../pages/api/auth/[...nextauth]';
+import Login from '../components/Login';
 
 export default async function RootLayout({
   children,
@@ -17,16 +18,21 @@ export default async function RootLayout({
       <head />
       <body>
         <SessionProvider session={session}>
-        <div className='flex'>
-          <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
-            <SideBar />
-          </div>
-          
-          {/* Client Provider - Notification */}
+          {!session ? (
+            <Login />
+          ) : (
+            <div className='flex'>
+              <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+                <SideBar />
+              </div>
 
-          <div className='bg-[#343541] flex-1'>{children}</div>
-        </div>
-        </SessionProvider>      
+              {/* Client Provider - Notification */}
+
+              <div className='bg-[#343541] flex-1'>{children}</div>
+            </div>
+          )}
+
+        </SessionProvider>
       </body>
     </html>
   )

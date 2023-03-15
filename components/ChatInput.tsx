76497ca98ae7d4d5cdd08a2be912f7a0f2931d7f
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-15 15:49:10 -0400

    Add React Hot Toast Library for notifications

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import React, { FormEvent, useState } from 'react'
 import { PaperAirplaneIcon } from '@heroicons/react/24/solid';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
-import React, { useState } from 'react'
+import { db } from '../firebase';
+import { toast } from 'react-hot-toast';
 
 type Props = {
   chatId: string;
@@ -14,9 +17,59 @@ function ChatInput({ chatId }: Props) {
 
   const [prompt, setPrompt] = useState("");
 
+  // TODO: useSWR to get model
+  const model = 'text-davinci-003';
+
+  const sendMessage = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (!prompt) return;
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      }
+    }
+
+    await addDoc(
+      collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'),
+      message
+    )
+
+    // Toast Notification Loading
+    const notification = toast.loading('ChatGPT is thinking...');
+
+    await fetch('/api/askQuestion', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session
+      }),
+    }).then(() => {
+      // Toast Notification Success
+      toast.success('ChatGPT has responded!', {
+        id: notification,
+      })
+    })
+  };
+
   return (
     <div className='bg-gray-700/50 text-gray-400 rounded-lg text-sm'>
-      <form className='p-5 space-x-5 flex'>
+      <form
+        className='p-5 space-x-5 flex'
+        onSubmit={sendMessage}
+      >
         <input
           className='bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300'
           disabled={!session}
